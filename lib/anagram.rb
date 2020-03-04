cfe46b19416cfd49cad26e672ccf364b7be4e4f1
Josh Ellis <joshe523@gmail.com>
2020-03-04 15:26:01 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+  def initialize(s)
+    @s = s
+  end
+
+  def match(list)
+    savedWord = @s.split('')
+    anagramList = []
+    list.each do |listWord|
+      anagramList << listWord if listWord.split('').sort == savedWord
+    end
+    return anagramList
+  end
